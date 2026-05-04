d39e2f928dc54dd3373ce4c8265fe6187237e51a
Ziadelsamanody <ziadelsamanody@gmail.com>
2026-05-04 04:08:05 +0300

    add checking dir of saving file exist or not

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,5 @@
 import cv2 as cv 
-
+import os 
 
 
 def read_video(video_path):
@@ -15,6 +15,10 @@ def read_video(video_path):
 
 
 def save_video(frames, output_path):
+    dir = os.path.dirname(output_path)
+    if  not os.path.exists(dir):
+        os.makedirs(output_path)
+    
     fourcc = cv.VideoWriter_fourcc(*'mp4v')
     out = cv.VideoWriter(output_path, fourcc, 30, (frames[0].shape[1], frames[0].shape[0]))
     for frame in frames:
